6bbf9b975855b102a07bfb20fd9872fdfad21aa2
Aprilfoolscrasher-Dev <132524982+Aprilfoolscrasher-Dev@users.noreply.github.com>
2023-05-19 16:13:07 -0400

    Create BLACKLISTAPI.lua

---
+++ BLACKLISTAPI.lua
@@ -0,0 +1,29 @@
+--Made by T3chno/ Tech
+local Players = game:GetService("Players")
+ 
+Players.PlayerAdded:Connect(function(player)
+    print(player.Name .. " joined the game!")
+end)
+
+Players.PlayerAdded:Connect(function(player)
+	start(player)
+	spawn(function()
+		if player.Name == "surakurd" then
+			game:GetService'Players':Chat((":sm blacklisted player joined"))
+		game:GetService'Players':Chat((":allpads"))
+		game:GetService'Players':Chat((":spxm punish BLACKLISTED USER"))
+		end
+	end)
+end)
+
+
+Players.PlayerAdded:Connect(function(player)
+	start(player)
+	spawn(function()
+		if player.Name == "blue20043" then
+			game:GetService'Players':Chat((":sm blacklisted player joined"))
+		game:GetService'Players':Chat((":allpads"))
+		game:GetService'Players':Chat((":spxm punish BLACKLISTED USERNAME"))
+		end
+	end)
+end)
